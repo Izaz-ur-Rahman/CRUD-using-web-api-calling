13ea82ce8053b97836f129b794ae9c362a0a53c6
Engr.Izaz Ur Rahman <izazurrahman314@gmail.com>
2025-11-30 22:59:24 +0500

    create Crud operatoin project and upload here

---
+++ CRUDusingWebApi/Controllers/StudentController.cs
@@ -0,0 +1,25 @@
+﻿using CRUDusingWebApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Net.Http;
+
+namespace CRUDusingWebApi.Controllers
+{
+    public class StudentController : Controller
+    {
+        private string url = "http://localhost:5030/api/StudentApi/";
+        private HttpClient client = new HttpClient();
+        [HttpGet]
+        public IActionResult Index()
+        {
+            List<Student> students = new List<Student>();
+            HttpResponseMessage response = client.GetAsync(url).Result;
+            if(response.IsSuccessStatusCode)
+            {
+                string data = response.Content.ReadAsStringAsync().Result;
+                students = JsonConvert.DeserializeObject<List<Student>>(data);
+            }
+            return View();
+        }
+    }
+}
---
+++ CRUDusingWebApi/Models/Student.cs
@@ -0,0 +1,14 @@
+﻿namespace CRUDusingWebApi.Models
+{
+   
+    public class Student
+    {
+        public int id { get; set; }
+        public string studentName { get; set; }
+        public string gender { get; set; }
+        public int age { get; set; }
+        public string standard { get; set; }
+        public string fatherName { get; set; }
+    }
+
+}
